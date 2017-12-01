6a97dffa87026cd8b7656ec22e6a8ec8ffd75da2
MacacMaster <MacacMaster@hotmail.com>
2017-12-01 15:19:15 -0500

    Found writeLog bug. It's my bad -M-A

--- Serveur/modules/PlanificationGlobale/PlanificationGlobale.py
+++ Serveur/modules/PlanificationGlobale/PlanificationGlobale.py
@@ -46,9 +46,9 @@ class Controleur():
         self.writeLog("Ouverture du Module")
 
         
-        self.listeFonctions=self.sql.selDonnees("nom")
+        #self.listeFonctions=self.sql.selDonnees("nom")
         
-        #self.sql.creerFonction("Sprint 1","FonctionNom","priorite","debut","fin")
+        self.sql.creerFonction('Sprint1','FonctionNom','priorite','debut','fin')
         self.sql.calculPourcent()
         self.vue.root.mainloop()
         
